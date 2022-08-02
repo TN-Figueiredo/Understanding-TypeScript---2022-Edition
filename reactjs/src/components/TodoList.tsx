3b425e6e5578d1496ba01ea2f19671c19c575477
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-08-02 11:00:20 -0300

    feat: react typescript

---
+++ reactjs/src/components/TodoList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Todo } from "../model/todos";
+import "../css/TodoList.css";
+
+interface TodoListProps {
+  items: Todo[];
+  onDeleteTodo: (id: string) => void;
+}
+
+const TodoList: React.FC<TodoListProps> = ({ items, onDeleteTodo }) => {
+  return (
+    <ul>
+      {items.map((todo) => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={onDeleteTodo.bind(null, todo.id)}>DELETE</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
